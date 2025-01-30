f5a2adf8c6fc860037531b36ec3512c500172bd4
jstenle <37110682+jstenle@users.noreply.github.com>
2025-01-30 13:14:49 -0300

    Move TestRunner e WebDriverManager para novos pacotes
    - TestRunner movido para o pacote com.runner.
    - WebDriverManager movido para o pacote com.utils.

--- cucumber-selenium-test/src/test/java/com/utils/WebDriverManager.java
+++ cucumber-selenium-test/src/test/java/com/utils/WebDriverManager.java
@@ -1,21 +1,29 @@
-package com.e2eTesting.utils;
+package com.utils;
+
+import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebDriverManager {
 	private WebDriver driver;
+	private WebDriverWait wait;
 
+	// Inicializa o WebDriver e define o tempo de espera padrão
 	public void initializeDriver() {
 		try {
+			// Define a propriedade do ChromeDriver
 			System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 			driver = new ChromeDriver();
+			wait = new WebDriverWait(driver, Duration.ofSeconds(30)); // Configura o tempo de espera
 		} catch (Exception e) {
 			System.err.println("Error initializing WebDriver: " + e.getMessage());
 			throw e;
 		}
 	}
 
+	// Retorna a instância do WebDriver
 	public WebDriver getDriver() {
 		if (driver == null) {
 			throw new IllegalStateException("WebDriver not initialized. Call initializeDriver() first.");
@@ -23,11 +31,9 @@ public class WebDriverManager {
 		return driver;
 	}
 
+	// Muda o foco para uma nova aba
 	public void changeTabFocus() {
-		// Armazenar a aba original
 		String originalTab = driver.getWindowHandle();
-
-		// Mudar para a nova aba
 		for (String tab : driver.getWindowHandles()) {
 			if (!tab.equals(originalTab)) {
 				driver.switchTo().window(tab);
@@ -36,9 +42,15 @@ public class WebDriverManager {
 		}
 	}
 
+	// Fecha o WebDriver
 	public void quitDriver() {
 		if (driver != null) {
 			driver.quit();
 		}
+	}
+
+	// Retorna a instância do WebDriverWait
+	public WebDriverWait getWait() {
+		return wait;
 	}
 }
